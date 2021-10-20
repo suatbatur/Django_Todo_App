54775122383aa29009105ee604631409fd5115d5
SuatBatur <msuatbatur@gmail.com>
2021-10-20 22:31:51 +0300

    todo app all finished

--- todo/views.py
+++ todo/views.py
@@ -7,10 +7,17 @@ def home(request):
 
 def todo_list(request):
     todos = Todo.objects.all()
+    form =TodoAddForm()
+    if request.method =="POST":
+        form = TodoAddForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("list")
+    
     
     context = {
-        "todos" : todos
-        
+        "todos" : todos,
+        "form" :form  
     
     }
     return render(request, "todo/todo_list.html",context)
@@ -40,6 +47,7 @@ def todo_update(request,id):
             return redirect("list")
     context ={
         "form" : form,
+        "todo" :todo
     }
     return render(request, "todo/todo_update.html", context)
 
