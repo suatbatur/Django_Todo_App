2d0be4130e2b85b2c1d4c544ea715a09fcbe85fc
SuatBatur <msuatbatur@gmail.com>
2021-10-20 20:19:26 +0300

    todo model created

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField( max_length=200)
+    completed =models.BooleanField(default=False)
+    created_date =models.DateTimeField( auto_now_add=True) #create edildiği tarihi alacak.
+
+    class Meta :
+        ordering = ("-created_date", )
+        
+    def __str__(self):
+        return self.title
+
